099a1e8800693905f067ba20a41804ca48bed1ef
nateos <sodjanathan@gmail.com>
2021-07-16 13:22:11 +0000

    added progress bar

--- src/components/SpaceStat.jsx
+++ src/components/SpaceStat.jsx
@@ -1,8 +1,20 @@
 import React from "react";
-import "../styles/_space-stat.scss"
 
 export default function SpaceStat() {
-	return (<section className="section">
-	  <div></div>
-  </section>);
+  return (
+    <section className="section">
+      <div>
+        <p className="description">
+          You've used <span className="">815 GB</span> of your storage
+        </p>
+        {/* Begin progress bar */}
+        <div class="progress-bar">
+          <div data-size="80" class="progress">
+            <div className="progress__indicator"></div>
+          </div>
+        </div>
+        {/* end progress bar */}
+      </div>
+    </section>
+  );
 }
