6105a833f81d3490db5383785c129358e649d346
nateos <sodjanathan@gmail.com>
2021-07-18 13:10:46 +0000

    positioning, additional classes

--- src/components/SpaceStat.jsx
+++ src/components/SpaceStat.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function SpaceStat() {
   return (
     <section className="section">
-      <div className="section__info">
         <p className="section__desc">
-          You've used <span className="storage-size">815 GB</span> of your storage
+          You've used <span className="storage-size">815 GB</span> of your
+          storage
         </p>
         {/* Begin progress bar */}
         <div class="section__progress-bar">
@@ -13,12 +13,16 @@ export default function SpaceStat() {
             <div className="section__progress-indicator"></div>
           </div>
         </div>
-			  {/* end progress bar */}
-			  <div className="section__caption flex">
-				  <p>0 GB</p>
-				  <p>1000 GB</p>
-			  </div>
-      </div>
+        {/* end progress bar */}
+        <div className="section__caption flex">
+          <p>0 GB</p>
+          <p>1000 GB</p>
+        </div>
+        <div className="section__storage-number">
+          <p class="section__paragraph">
+            <span>185</span>GB LEFT
+          </p>
+        </div>
     </section>
   );
 }
